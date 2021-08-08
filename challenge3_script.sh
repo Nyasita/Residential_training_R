9929823adc844e80d959847e10539090b894ef7d
Nyasita <72727098+Nyasita@users.noreply.github.com>
2021-08-08 15:20:20 +0300

    bash script for the basics: gapminder data

--- challenge3_script.sh
+++ challenge3_script.sh
@@ -3,6 +3,6 @@
 wget https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder_data.csv
 #create a data folder
 mkdir data
-#move the gapminder file to data
+#move the gapminder file to data folder
 mv gapminder_data.csv ./data
 
